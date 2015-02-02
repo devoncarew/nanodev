9252c4aeeacb41e53cdcff3c3730682f662fff4b
Devon Carew <devoncarew@gmail.com>
2015-02-01 18:24:32 -0800

    adjust circle configuration

--- tool/circleci.sh
+++ tool/circleci.sh
@@ -3,6 +3,20 @@
 # Fast fail the script on failures.
 set -e
 
+# Get the Dart SDK.
+DART_DIST=dartsdk-linux-x64-release.zip
+curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
+unzip $DART_DIST > /dev/null
+rm $DART_DIST
+export DART_SDK="$PWD/dart-sdk"
+export PATH="$DART_SDK/bin:$PATH"
+
+# Display installed versions.
+dart --version
+
+# Get our packages.
+pub get
+
 # Verify that the libraries are error free.
 pub global activate tuneup
 pub global run tuneup check
